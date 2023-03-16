7dacade6bd676f84705dcf4108db14a0c32e638e
DamianoCasolari <damianocasolari@gmail.com>
2023-03-16 17:45:20 +0100

    add play even or odd

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,28 +9,68 @@
 // console.log(turnWord);
 
 
-const userName = prompt("Inserisci una parola per veirificare che sia palindromo")
-let turnWord = "";
+// const userName = prompt("Inserisci una parola per veirificare che sia palindromo")
 
-function checkPalindrome(userName) {
+// function checkPalindrome(word) {
+//     let turnWord = "";
 
-    for (let index = userName.length - 1; index > -1; index--) {
-        const element = userName[index];
-        console.log(element);
-        turnWord += element;
-    }
-    console.log(turnWord);
-   
+//     for (let index = word.length - 1; index > -1; index--) {
+//         const element = word[index];
+//         console.log(element);
+//         turnWord += element;
+//     }
+    
+//     console.log(turnWord);
+//     return turnWord
+    
+// }
+
+// let reverseWord = checkPalindrome(userName);
+
+
+// if (userName == "") {
+//     document.getElementsByTagName("h2")[0].innerHTML = `Non hai inserito nessuna parola`
+// } else if (userName == reverseWord) {
+//     document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) è un Palindromo`
+// } else {
+//     document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) NON è un Palindromo`
+// }
+
+
+// Even or odd play 
+
+const evenOrOdd =  prompt("Scegli pari o dispari?").toLocaleLowerCase()
+const chooseANumber = Number(prompt("Scegli un numero da 1 a 5"))
+
+function randomNumber () {
+    let pcNumber = Math.floor(Math.random() * 5 ) + 1 //Math.random includes 0 exludes 1
+    return pcNumber;
 }
 
+console.log(randomNumber());
+let pcNumber = randomNumber();
 
+function result( num1, num2){
+    let sum =  num1 + num2;
+    if (sum % 2 == 0) {
+        return "pari"
 
-checkPalindrome(userName);
+    } else {
+        return "dispari"
+    }
+}
+let finalResult = result(chooseANumber,pcNumber )
+let playerPoints = 0;
+let pcPoints = 0;
 
-if (userName == "") {
-    document.getElementsByTagName("h2")[0].innerHTML = `Non hai inserito nessuna parola`
-} else if (userName == turnWord) {
-    document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) è un Palindromo`
+if (finalResult == evenOrOdd) {
+    document.querySelector(".result").innerHTML = "You win"
+    document.querySelector(".player").innerHTML = `Player ${++playerPoints} `
+    document.querySelector(".current_game_points_player").innerHTML = chooseANumber
+    document.querySelector(".current_game_points_pc").innerHTML = pcNumber
 } else {
-    document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) NON è un Palindromo`
+    document.querySelector(".result").innerHTML = "You Lost"
+    document.querySelector(".computer").innerHTML = `computer ${++pcPoints} `
+    document.querySelector(".current_game_points_player").innerHTML = chooseANumber
+    document.querySelector(".current_game_points_pc").innerHTML = pcNumber
 }
