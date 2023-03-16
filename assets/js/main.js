ce861f93c54a97b69b3be5b77fef9d15e50b61d7
DamianoCasolari <damianocasolari@gmail.com>
2023-03-16 16:17:36 +0100

    exercise palindrome

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
+// const userName = prompt("Inserisci una parola per veirificare che sia palindromo")
+// let turnWord = "";
+
+// for (let index = 0; index < userName.length; index++) {
+//     const element = userName[index];
+//     console.log(element);
+//     turnWord += element;
+// }
+// console.log(turnWord);
+
+
 const userName = prompt("Inserisci una parola per veirificare che sia palindromo")
 let turnWord = "";
 
-for (let index = Number(userName.length); index < 0; index--) {
-    const element = userName[index];
-    console.log(element);
-    
+function checkPalindrome(userName) {
+
+    for (let index = userName.length - 1; index > -1; index--) {
+        const element = userName[index];
+        console.log(element);
+        turnWord += element;
+    }
+    console.log(turnWord);
+   
+}
+
+
+
+checkPalindrome(userName);
+
+if (userName == "") {
+    document.getElementsByTagName("h2")[0].innerHTML = `Non hai inserito nessuna parola`
+} else if (userName == turnWord) {
+    document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) è un Palindromo`
+} else {
+    document.getElementsByTagName("h2")[0].innerHTML = `il nome inserito (${userName}) NON è un Palindromo`
 }
